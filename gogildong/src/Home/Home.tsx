37c8f4ff5ab9f5f70a40ec4dac2b5afd0fcec566
wys0530 <youn.soo.woo@gmail.com>
2025-11-04 00:24:35 +0900

    feat: 내비게이션 바 구현

---
+++ gogildong/src/Home/Home.tsx
@@ -0,0 +1,16 @@
+// src/Home/Home.tsx
+import React from "react";
+import NavBar from "../components/Common/NavBar";
+import type { NavKey } from "../components/Common/NavBar"; // 👈 타입 전용 import
+
+function Home() {
+  const [active, setActive] = React.useState<NavKey>("home");
+
+  return (
+    <div className="w-91 h-223 flex flex-col justify-end items-center pb-10 bg-slate-100">
+      <NavBar active={active} onChange={setActive} />
+    </div>
+  );
+}
+
+export default Home;
