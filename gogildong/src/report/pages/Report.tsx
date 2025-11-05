40e6e38a4de8c18c67eaf40fafe5977723313e49
otwaylee <ahyeong858@gmail.com>
2025-11-05 10:57:21 +0900

    remove: report 디렉토리 삭제

--- gogildong/src/report/pages/Report.tsx
+++
@@ -1,9 +0,0 @@
-import LocationCard from '../components/LocationCard';
-
-export default function Report() {
-  return (
-    <div className='bg-'>
-      <LocationCard title='1-A' date='202020' location='asd' />
-    </div>
-  );
-}
