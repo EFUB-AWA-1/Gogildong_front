31266801022172066d30e0c068cbb3c6813d0c68
otwaylee <ahyeong858@gmail.com>
2025-11-05 12:40:57 +0900

    feat: 제보 페이지 - 학교 페이지에서 연결

---
+++ gogildong/src/Report/pages/Report.tsx
@@ -0,0 +1,55 @@
+import Header from '@/common/components/Header';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ActionButton from '@/common/components/ActionButton';
+
+import Step0 from './steps/Step0';
+import Step1 from './steps/Step1';
+import Step2 from './steps/Step2';
+import Step3 from './steps/Step3';
+
+export default function Report() {
+  const navigate = useNavigate();
+  const [step, setStep] = useState(0);
+  const handleNext = () => setStep((prev) => Math.min(prev + 1, 3));
+  const handlePrev = () => {
+    if (step === 0) navigate(-1);
+    else setStep((prev) => prev - 1);
+  };
+
+  const titles = ['제보하기', '1/3', '2/3', '3/3'];
+  const buttonLabel = step === 0 ? '선택' : step === 3 ? '촬영하기' : '다음';
+  const renderStep = () => {
+    switch (step) {
+      case 0:
+        return <Step0 />;
+
+      case 1:
+        return <Step1 />;
+      case 2:
+        return <Step2 />;
+      case 3:
+        return <Step3 />;
+    }
+  };
+  return (
+    <div
+      className={`flex flex-col h-full items-center justify-between ${
+        step === 0 ? 'bg-white' : 'bg-black'
+      }`}>
+      <Header
+        title={titles[step]}
+        onBackClick={handlePrev}
+        darkMode={step !== 0}
+      />
+
+      <div className='w-full my-20 flex flex-1 justify-center items-center'>
+        {renderStep()}
+      </div>
+
+      <div className='w-full fixed bottom-0 px-6 py-4'>
+        <ActionButton label={buttonLabel} onClick={handleNext} />
+      </div>
+    </div>
+  );
+}
--- gogildong/src/router.tsx
+++ gogildong/src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from './common/layout/MainLayout';
 import Home from './Home/pages/Home';
 import School from './School/pages/School';
+import Report from './Report/pages/Report';
 
 const router = createBrowserRouter([
   {
@@ -9,9 +10,13 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { path: '/home', element: <Home /> },
-      { path: '/school', element: <School /> },
+      {
+        path: '/school',
+        element: <School />,
+      },
     ],
   },
+  { path: '/school/report', element: <Report /> },
 ]);
 
 export default router;
