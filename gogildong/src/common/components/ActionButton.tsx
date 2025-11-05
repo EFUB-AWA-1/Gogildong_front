8a4bdb2b9d77c6041ceede390df6f1ba8ae078f8
wys0530 <youn.soo.woo@gmail.com>
2025-11-06 02:23:29 +0900

    refactor: 액션 버튼 disabled 상태 추가

--- gogildong/src/common/components/ActionButton.tsx
+++ gogildong/src/common/components/ActionButton.tsx
@@ -2,18 +2,24 @@ interface ActionButtonProps {
   label: string;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
 export default function ActionButton({
   label,
   className,
   onClick,
+  disabled = false,
 }: ActionButtonProps) {
   return (
     <>
       <button
         onClick={onClick}
-        className={`bg-neon-100 w-full py-3.5 rounded-3xl ${className}`}>
+        disabled={disabled}
+        className={`w-full py-3.5 rounded-3xl ${className} ${
+          disabled ? "bg-gray-20 " : "bg-neon-100 cursor-pointer"
+        } text-black text-base font-bold`}
+      >
         {label}
       </button>
     </>
