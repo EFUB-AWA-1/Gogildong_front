77339af28e46d981959f5aead7abe027bb6aa437
wys0530 <youn.soo.woo@gmail.com>
2025-11-06 20:38:56 +0900

    feat: 지도 마커 추가

--- gogildong/src/Home/pages/Home.tsx
+++ gogildong/src/Home/pages/Home.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import NavBar from "../../common/components/NavBar";
 import type { NavKey } from "../../common/components/NavBar";
 import SearchBar from "../components/SearchBar";
+import customMarker from "../assets/icon_marker.svg";
 
 declare global {
   interface Window {
@@ -17,13 +18,61 @@ export default function Home() {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    const position = new kakao.maps.LatLng(37.5611732, 126.9428198);
+    const position = new kakao.maps.LatLng(37.5618588, 126.9468339);
     const options = {
       center: position,
       level: 3,
     };
 
-     new kakao.maps.Map(mapRef.current, options);
+    const map = new kakao.maps.Map(mapRef.current, options);
+
+    const markerImage = new kakao.maps.MarkerImage(
+      customMarker,
+      new kakao.maps.Size(48, 48),
+      { offset: new kakao.maps.Point(24, 24) }
+    );
+
+    const bounds = new kakao.maps.LatLngBounds();
+
+    const markerPositions = [
+      {
+        title: "이화여자대학교부속초등학교",
+        lat: 37.5611732,
+        lng: 126.9428198,
+      },
+      {
+        title: "세브란스병원",
+        lat: 37.5623371,
+        lng: 126.9408692,
+      },
+    ];
+
+    markerPositions.forEach((data) => {
+      const pos = new kakao.maps.LatLng(data.lat, data.lng);
+      new kakao.maps.Marker({
+        map: map,
+        position: pos,
+        title: data.title,
+        image: markerImage,
+      });
+      bounds.extend(pos);
+    });
+
+    if (markerPositions.length === 1) {
+      const { lat, lng } = markerPositions[0];
+      map.panTo(new kakao.maps.LatLng(lat, lng));
+    } else if (markerPositions.length > 1) {
+      map.setBounds(bounds);
+    }
+    {
+      /*}
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+      image: markerImage,
+    });
+
+    marker.setMap(map);*/
+    }
   }, []);
 
   return (
