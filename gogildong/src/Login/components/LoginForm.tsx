3a0544ecf1ca315ebbf7622f6d7c4683cd0fd08b
otwaylee <ahyeong858@gmail.com>
2025-11-23 02:29:36 +0900

    feat: 로그인 성공 로직 구현

--- gogildong/src/Login/components/LoginForm.tsx
+++ gogildong/src/Login/components/LoginForm.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import LoginInputField from "./LoginInputField";
-import { signInUser } from "../api/signInUser";
 import ActionButton from "@/common/components/ActionButton";
+import { useState } from "react";
+import { signInUser } from "../api/signInUser";
+import LoginInputField from "./LoginInputField";
 import LoginSupportLinks from "./LoginSupportLinks";
+import { useAuthStore } from "../stores/useAuthStore";
+import { useNavigate } from "react-router-dom";
 
 const INITIAL_VALUES = { id: "", password: "" };
 
 export default function LoginForm() {
+  const navigate = useNavigate();
   const [values, setValues] = useState(INITIAL_VALUES);
   const [error, setError] = useState<string | null>(null);
 
@@ -29,7 +32,9 @@ export default function LoginForm() {
         loginId: values.id,
         password: values.password
       };
-      await signInUser(payload);
+      const res = await signInUser(payload); 
+      useAuthStore.getState().setTokens(res.accessToken, res.refreshToken);
+      navigate("/home");
     } catch (err) {
       setError("아이디 또는 비밀번호가 일치하지 않습니다.");
       console.log(err);
@@ -51,7 +56,7 @@ export default function LoginForm() {
           onChange={handleChange("password")}
         />
         {error && <p className="text-sm text-warning-100">{error}</p>}
-        <div className="flex justify-center">
+        <div className="mt-6 flex justify-center">
           <LoginSupportLinks />
         </div>
       </div>
