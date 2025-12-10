66820fdb9a8c019a55f3ade4bae02a7f9b02fec4
otwaylee <ahyeong858@gmail.com>
2025-12-10 19:34:39 +0900

    feat: 시설 정보 조회 api - 건물 / 층 / 시설 / 도면 정보 GET

---
+++ gogildong/src/Report/api/getFacilities.ts
@@ -0,0 +1,23 @@
+import axiosInstance from '@/common/api/axiosInstance';
+
+export const getBuildings = async () => {
+  const { data } = await axiosInstance.get('/buildings');
+  return data;
+};
+
+export const getFloorsByBuilding = async (buildingId: number) => {
+  const { data } = await axiosInstance.get(`/building/${buildingId}/floor`);
+  return data;
+};
+
+export const getFacilitiesByFloor = async (floorId: number) => {
+  const { data } = await axiosInstance.get(`/floor/${floorId}/facilities`);
+  return data;
+};
+
+export const getFloorPlan = async (schoolId: number, floorId: number) => {
+  const { data } = await axiosInstance.get(
+    `/schools/${schoolId}/floors/${floorId}/plan`
+  );
+  return data;
+};
