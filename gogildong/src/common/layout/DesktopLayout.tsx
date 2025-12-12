310cfe95e3d700458f38bb402581f1ce54a4a380
otwaylee <ahyeong858@gmail.com>
2025-12-12 09:20:22 +0900

    design: pc 버전 반응형 사이드바 완성

--- gogildong/src/common/layout/DesktopLayout.tsx
+++ gogildong/src/common/layout/DesktopLayout.tsx
@@ -5,12 +5,12 @@ export default function DesktopLayout() {
   return (
     <div className="mx-auto flex min-h-screen max-w-[1920px] min-w-7xl">
       {/* 왼쪽 내비게이션 */}
-      <aside className="flex w-[19%] shrink-0 flex-col items-center bg-white">
+      <aside className="flex w-[19%] flex-col bg-white">
         <DesktopSidebar />
       </aside>
 
       {/* 오른쪽 컨텐츠 영역 (배경 + 기본 패딩) */}
-      <main className="flex-1 bg-[#F5F5F5] px-25 py-15">
+      <main className="flex-1 bg-gray-10 px-25 py-15">
         <Outlet />
       </main>
     </div>
